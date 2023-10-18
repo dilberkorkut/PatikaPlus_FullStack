31239bc3cd8bdf4e93eeb4d004fac26ba1310c88
¨Dilber <dilberkrkt@mail.com>
2023-10-18 08:27:55 +0300

    Dosyadan Sayilari Okuma Yazdirma

---
+++ Week_6/DosyadanSayilariOkumaVeToplama/src/Main.java
@@ -0,0 +1,57 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args, Object sayilar) {
+        //Dosya Olusturma
+        File file = new File("sayilar.txt");
+        if(file.exists()) {
+            System.out.println("DOsya mevcut");
+        } else {
+            System.out.println("Dosya mevcut degil");
+        }
+
+
+        try {
+           FileWriter fileWriter = new FileWriter(file, false);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            bufferedWriter.write("5");
+            bufferedWriter.newLine();
+            bufferedWriter.write("10");
+            bufferedWriter.newLine();
+            bufferedWriter.write("20");
+            bufferedWriter.newLine();
+            bufferedWriter.write("12");
+            bufferedWriter.newLine();
+            bufferedWriter.write("33");
+            bufferedWriter.newLine();
+            bufferedWriter.close();
+
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            int sum = 0;
+
+            while((line = bufferedReader.readLine()) != null) {
+                sum += Integer.parseInt(line);
+            }
+            bufferedReader.close();
+            System.out.println(" Toplam : " + sum);
+
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
