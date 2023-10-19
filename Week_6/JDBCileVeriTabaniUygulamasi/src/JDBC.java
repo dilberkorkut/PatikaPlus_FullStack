4bfbd117a02ba42d789197974612fe62cddf51c8
¨Dilber <dilberkrkt@mail.com>
2023-10-19 03:09:29 +0300

    JDBC

---
+++ Week_6/JDBCileVeriTabaniUygulamasi/src/JDBC.java
@@ -0,0 +1,28 @@
+import java.sql.*;
+
+public class JDBC {
+
+    public static final String DB_URL = "jdbc:mysql://localhost/patika";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "mysql";
+
+    public static void main(String[] args) {
+
+
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME,DB_PASSWORD);
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+
+            }
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());;
+        }
+    }
+}
