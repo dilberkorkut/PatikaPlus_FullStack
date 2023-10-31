cd1ef1eba904f42a6f66b6cd45158b17e559aee3
Dilber Korkut Bilgin <108426157+dilberkorkut@users.noreply.github.com>
2023-10-31 23:53:44 +0300

    Delete Week_8 directory
    ..

--- Week_8/TravelAgency/src/com/patikadev/View/EmployeeGUI.java
+++
@@ -1,7 +0,0 @@
-package com.patikadev.View;
-
-import javax.swing.*;
-
-public class EmployeeGUI {
-    private JPanel wrapper;
-}
--- Week_8/TravelAgency/src/com/patikadev/View/LoginGUI.java
+++
@@ -1,7 +0,0 @@
-package com.patikadev.View;
-
-import javax.swing.*;
-
-public class LoginGUI {
-    private JPanel wrapper;
-}
